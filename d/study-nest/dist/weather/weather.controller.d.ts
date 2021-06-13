f721dcd2e1c6d62f91f0c92a5b2a38382193f562
jaesung <japark@pasona.tech>
2021-06-13 09:37:24 +0900

    delete folder

--- d/study-nest/dist/weather/weather.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { AppService, PrismaService } from 'src/app.service';
-export declare class WeatherController {
-    private readonly appService;
-    private readonly prisma;
-    constructor(appService: AppService, prisma: PrismaService);
-    getWeather(res: any): void;
-    getDBWeather(res: any): Promise<void>;
-    setDBData(): Promise<void>;
-}
